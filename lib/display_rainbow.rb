8e817b520a129e81f34178ac2cc66a2d3936d2b7
Natalie Tagher <natalie.tagher@wework.com>
2018-02-20 18:58:14 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 def display_rainbow (color)
 color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-letters = {"R", "O", "Y", "G", "B", "I", "V"}
+letters = {"r", "o", "y", "g", "b", "i", "v"}
 puts "R: #{color[0]}, O:#{color[1]},Y:#{color[2]}, G:#{color[3]},B:#{color[4]},I:#{color[5]}, V:#{color[6]}"
 puts "alphabetical order would be: #{letters[4]}, #{letters[3]}, #{letters[5]}, #{letters[1]}, #{letters[0]}, #{letters[6]}, #{letters[2]}"
 end
